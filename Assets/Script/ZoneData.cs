d2fe8bee606c92152d935bbd8d9ec921738bd884
Anatano <165888612+Anatanoooooooooo@users.noreply.github.com>
2026-03-31 21:39:51 +0200

    Delete Assets directory

--- Assets/Script/ZoneData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[System.Serializable]
-public class ZoneData
-{
-    public string zoneName;            // Nom de la zone
-    public Texture texture360;         // Image 360° de cette zone
-    public HotspotData[] hotspots;     // Liste des hotspots dans cette zone
-}
